7c51f6a235919faef321c00d0d5bcc43ecc0433e
LiferMatrix <daniel_bh_mg@hotmail.com>
2025-11-05 22:04:43 -0300

    Update StochEma2.js

--- StochEma2.js
+++ StochEma2.js
@@ -18,7 +18,7 @@ const config = {
   STOCHASTIC_PERIOD_K: 5,
   STOCHASTIC_SMOOTH_K: 3,
   STOCHASTIC_PERIOD_D: 3,
-  STOCHASTIC_BUY_MAX: 65, // Limite máximo para compra (4h e Diário) - Ajustado para maior assertividade
+  STOCHASTIC_BUY_MAX: 75, // Limite máximo para compra (4h e Diário) - Ajustado para maior assertividade
   STOCHASTIC_SELL_MIN: 80, // Limite mínimo para venda (4h e Diário) - Ajustado para maior assertividade
   LSR_BUY_MAX: 2.5, // Limite máximo de LSR para compra
   LSR_SELL_MIN: 2.6, // Limite mínimo de LSR para venda
