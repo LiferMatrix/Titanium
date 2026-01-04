7309e13a5d7135693adb50b774b21d9310e86a22
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-01-04 17:01:07 -0300

    Update EMARLTOP.js

--- EMARLTOP.js
+++ EMARLTOP.js
@@ -6,8 +6,8 @@ const { SMA, EMA, RSI, Stochastic, ATR, ADX, CCI } = require('technicalindicator
 if (!globalThis.fetch) globalThis.fetch = fetch;
 
 // === CONFIGURE AQUI SEU BOT E CHAT ===
-const TELEGRAM_BOT_TOKEN = '8010060485:AAESqJMqL0J5OE';
-const TELEGRAM_CHAT_ID = '-100255';
+const TELEGRAM_BOT_TOKEN = '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A';
+const TELEGRAM_CHAT_ID = '-1001990889297';
 
 
 // === CONFIGURAÇÕES DE OPERAÇÃO ===
