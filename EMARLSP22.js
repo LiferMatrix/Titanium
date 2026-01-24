8b59dc90a1293581e4299e406ac65138aaee9be0
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-01-24 17:26:59 -0300

    Update EMARLSP22.js

--- EMARLSP22.js
+++ EMARLSP22.js
@@ -6,8 +6,8 @@ const { Stochastic, EMA, RSI, ATR } = require('technicalindicators');
 if (!globalThis.fetch) globalThis.fetch = fetch;
 
 // === CONFIGURE AQUI SEU BOT E CHAT ===
-const TELEGRAM_BOT_TOKEN = '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg';
-const TELEGRAM_CHAT_ID = '-1002554953979';
+const TELEGRAM_BOT_TOKEN = '7708427979:AAF7vVx6AG8g';
+const TELEGRAM_CHAT_ID = '-1002';
 
 // === DIRETÓRIOS ===
 const LOG_DIR = './logs';
