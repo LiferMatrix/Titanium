a5063df367e46626cebcbae1ed10ce96a5f4752b
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-01-26 12:42:09 -0300

    Update EMARLSP24.js

--- EMARLSP24.js
+++ EMARLSP24.js
@@ -145,8 +145,8 @@ if (!globalThis.fetch) {
 }
 
 // === CONFIGURE AQUI SEU BOT E CHAT ===
-const TELEGRAM_BOT_TOKEN = '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg';
-const TELEGRAM_CHAT_ID = '-1002554953979';
+const TELEGRAM_BOT_TOKEN = '7708427979:AAF7vVx6g';
+const TELEGRAM_CHAT_ID = '-10029';
 
 // === DIRETÓRIOS ===
 const LOG_DIR = './logs';
