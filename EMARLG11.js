d1200b7c0ad0ac9692e5dd65161e848e46b67c1b
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-06 06:24:24 -0300

    Update EMARLG11.js

--- EMARLG11.js
+++ EMARLG11.js
@@ -6,8 +6,8 @@ const { EMA, RSI, ATR } = require('technicalindicators');
 if (!globalThis.fetch) globalThis.fetch = fetch;
 
 // === CONFIGURE AQUI SEU BOT E CHAT ===
-const TELEGRAM_BOT_TOKEN = '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A';
-const TELEGRAM_CHAT_ID = '-1001990889297';
+const TELEGRAM_BOT_TOKEN = '7633398974:AAHaVFs_D_oo4N0A';
+const TELEGRAM_CHAT_ID = '-100197';
 
 
 // === DIRETÓRIOS ===
