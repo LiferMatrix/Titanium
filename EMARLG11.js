dd1d16cb6576cc800be35c8af9400e011b88d99c
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-10 07:49:40 -0300

    Update EMARLG11.js

--- EMARLG11.js
+++ EMARLG11.js
@@ -9,8 +9,8 @@ if (!globalThis.fetch) globalThis.fetch = fetch;
 // =====================================================================
 
 // === CONFIGURE AQUI SEU BOT E CHAT ===
-const TELEGRAM_BOT_TOKEN = '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg';
-const TELEGRAM_CHAT_ID = '-1002554953979';
+const TELEGRAM_BOT_TOKEN = '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A';
+const TELEGRAM_CHAT_ID = '-1001990889297';
 
 // === CONFIGURAÇÃO DO ESTOCÁSTICO ===
 const STOCHASTIC_CONFIG = {
