6cda1804af8b5c075c6de7e10ab157f5e82e4c44
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-12 19:45:33 -0300

    Update EMARL112.js

--- EMARL112.js
+++ EMARL112.js
@@ -12,8 +12,8 @@ if (!globalThis.fetch) globalThis.fetch = fetch;
 const CONFIG = {
     TELEGRAM: {
         // === CONFIGURE AQUI SEU BOT E CHAT ===
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:A4N0A',
+        CHAT_ID: '-10017'
     },
     STOCHASTIC: {
         ENABLED: true,
