330a64f7ceea5c2ceee0ea5420c9871afe97820a
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-13 20:19:32 -0300

    Update EMARLG22.js

--- EMARLG22.js
+++ EMARLG22.js
@@ -25,8 +25,8 @@ const RSI_1H_CONFIG = {
 
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAFdg',
+        CHAT_ID: '-10025'
     },
     STOCHASTIC: {
         ENABLED: true,
