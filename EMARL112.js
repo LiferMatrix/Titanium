798aa3d2a80b6f2471d838d6a4e79e99a0702c56
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-13 19:46:33 -0300

    Update EMARL112.js

--- EMARL112.js
+++ EMARL112.js
@@ -25,8 +25,8 @@ const RSI_1H_CONFIG = {
 
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVFs_D_oZA',
+        CHAT_ID: '-1001'
     },
     STOCHASTIC: {
         ENABLED: true,
