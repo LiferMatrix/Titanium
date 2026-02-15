8d845fbf1dd0f3e4ce74bce248efddef0c3eac7f
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-15 18:28:15 -0300

    Update EMARLtotal11.js

--- EMARLtotal11.js
+++ EMARLtotal11.js
@@ -188,8 +188,8 @@ const RSI_1H_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVFs_D_oZA',
+        CHAT_ID: '-100199'
     },
     STOCHASTIC: {
         ENABLED: true,
