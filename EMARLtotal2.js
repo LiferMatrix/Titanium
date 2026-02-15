d63c2c5f8221ab3547c4bf499fff1356bb6b615e
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-15 10:33:46 -0300

    Update EMARLtotal2.js

--- EMARLtotal2.js
+++ EMARLtotal2.js
@@ -38,8 +38,8 @@ const rsi1hMonitorCache = {};
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSg',
+        CHAT_ID: '-100255'
     },
     STOCHASTIC: {
         ENABLED: true,
