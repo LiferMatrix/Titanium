da5c530098c42ee061ce5a4181a4b9a6581efc38
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-15 18:46:03 -0300

    Update Telegram bot configuration values
    Updated Telegram bot token and chat ID in configuration.

--- EMARLtotal2.js
+++ EMARLtotal2.js
@@ -188,8 +188,8 @@ const RSI_1H_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG8pdg',
+        CHAT_ID: '-1002559'
     },
     STOCHASTIC: {
         ENABLED: true,
