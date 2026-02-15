6ac58d94b03db441d00f5fa80d5a81b8e34c4bec
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-15 17:32:03 -0300

    Update Telegram bot token and chat ID

--- EMARLtotal2.js
+++ EMARLtotal2.js
@@ -23,8 +23,8 @@ const RSI_1H_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6g',
+        CHAT_ID: '-100255'
     },
     STOCHASTIC: {
         ENABLED: true,
