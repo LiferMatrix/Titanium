ff096dc3a4c9bfca598ff93de1ec1300f9015676
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-19 09:10:01 -0300

    Modify Telegram bot credentials
    Updated Telegram bot token and chat ID in configuration.

--- EMARLtotal115.js
+++ EMARLtotal115.js
@@ -228,8 +228,8 @@ const VOLUME_1H_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG',
+        CHAT_ID: '-1002554'
     },
     STOCHASTIC: {
         ENABLED: true,
