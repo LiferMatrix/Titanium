eee8ae14e779a529696237ac5bb20059cab8e220
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-19 17:29:00 -0300

    Modify Telegram bot configuration
    Updated Telegram bot token and chat ID in configuration.

--- CCI111.js
+++ CCI111.js
@@ -226,8 +226,8 @@ const VOLUME_1H_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVFs_D_oZf',
+        CHAT_ID: '-10019'
     },
     CCI: {
         ENABLED: true,
