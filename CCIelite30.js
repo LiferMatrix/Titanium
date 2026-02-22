8c67f89250dea167cdffcc0a774e9ac1dac3b115
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-22 07:27:27 -0300

    Update CCIelite30.js

--- CCIelite30.js
+++ CCIelite30.js
@@ -254,8 +254,8 @@ const VOLUME_3M_CONFIG = {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVFs_D_',
+        CHAT_ID: '-1001'
     },
 
     CCI: {
