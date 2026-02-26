9c412f28432351d976577c26893d098888408eab
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-02-26 16:20:06 -0300

    Update CCITrade.js

--- CCITrade.js
+++ CCITrade.js
@@ -239,8 +239,8 @@ const CCISignalSchema = z.object({
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG',
+        CHAT_ID: '-1002554'
     },
 
     CCI: {
