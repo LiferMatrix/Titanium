5a64c54b0c8db5df6ca5f5283a8bcf0b5963ce7a
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-03-01 13:52:32 -0300

    Update V3.js

--- V3.js
+++ V3.js
@@ -10,8 +10,8 @@ if (!globalThis.fetch) globalThis.fetch = fetch;
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVFsA',
+        CHAT_ID: '-100197'
     },
     PERFORMANCE: {
         SYMBOL_DELAY_MS: 200,
