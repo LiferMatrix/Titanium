7507e470482950e8d495a642749fb077d159c29b
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-03-24 09:23:30 -0300

    Update BBweb.js

--- BBweb.js
+++ BBweb.js
@@ -10,8 +10,8 @@ if (!globalThis.fetch) globalThis.fetch = fetch;
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7633398974:AAHaVFs_D_oZfswILgUd0i2wHgF88fo4N0A',
-        CHAT_ID: '-1001990889297'
+        BOT_TOKEN: '7633398974:AAHaVA',
+        CHAT_ID: '-1001997'
     },
     SCAN: {
         BATCH_SIZE: 10,
