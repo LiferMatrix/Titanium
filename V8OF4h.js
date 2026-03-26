7f8f756b2fd58f29a7f23c3434e2827cc3ef6c16
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-03-26 07:41:35 -0300

    Update V8OF4h.js

--- V8OF4h.js
+++ V8OF4h.js
@@ -12,8 +12,8 @@ if (!globalThis.fetch) globalThis.fetch = fetch;
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG8pS
+        CHAT_ID: '-100255
     },
     PERFORMANCE: {
         SYMBOL_DELAY_MS: 250,
