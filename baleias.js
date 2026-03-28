1ad7893c058044f3843edb72234aebf6184b0f99
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-03-28 15:30:15 -0300

    Update baleias.js

--- baleias.js
+++ baleias.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG8pS
+        CHAT_ID: '-1002554
     },
     WHALE_HUNTING: {
         CVD_THRESHOLD_CRITICAL: 500000,
