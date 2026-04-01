fa0667a30e456e4a92b0c8bc810e2f783e414c25
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-04-01 17:40:42 -0300

    Update Telegram bot token and chat ID

--- FundX2.js
+++ FundX2.js
@@ -884,8 +884,8 @@ class RealCVDManager {
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AGg',
+        CHAT_ID: '-1002559'
     },
     MONITOR: {
         INTERVAL_MINUTES: 15,
