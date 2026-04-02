5ce39afbfa5e15eae43edd7c9399137697e8afc0
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-04-02 08:42:45 -0300

    Update FundX3.js

--- FundX3.js
+++ FundX3.js
@@ -16,8 +16,8 @@ const ALERT_COUNTERS_FILE = path.join(__dirname, 'alertCounters.json');
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vVx6AG8g',
+        CHAT_ID: '-100255'
     },
     MONITOR: {
         INTERVAL_MINUTES: 15,
