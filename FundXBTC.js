1f696abe483647a41b7a541309f7b844bf81ed1c
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-04-15 21:01:58 -0300

    Update FundXBTC.js

--- FundXBTC.js
+++ FundXBTC.js
@@ -16,8 +16,8 @@ const BTC_ALERTED_FILE = path.join(__dirname, 'btcAlertedHistory.json');
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979'
+        BOT_TOKEN: '7708427979:AAF7vV',
+        CHAT_ID: '-100259'
     },
     MONITOR: {
         SCAN_INTERVAL_SECONDS: 60,
