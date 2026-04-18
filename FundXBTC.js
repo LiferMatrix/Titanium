1858465bd9a09a7613a71bb41913cf0ec4c68c2c
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-04-17 22:25:31 -0300

    Update FundXBTC.js

--- FundXBTC.js
+++ FundXBTC.js
@@ -17,8 +17,8 @@ const VOLUME_MOMENTUM_FILE = path.join(__dirname, 'volumeMomentumMemory.json');
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979',
+        BOT_TOKEN: '7708427979:AAF7vVx6AG',
+        CHAT_ID: '-100255',
         MESSAGE_DELAY_MS: 13000,
         MAX_MESSAGES_PER_MINUTE: 20,
         BURST_DELAY_MS: 8000,
