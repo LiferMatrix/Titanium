751005593a9bf55bb753a6523ffebe3276e5d372
LiferMatrix <daniel_bh_mg@hotmail.com>
2026-04-23 10:04:50 -0300

    Update FVGWEBSMC.js

--- FVGWEBSMC.js
+++ FVGWEBSMC.js
@@ -26,8 +26,8 @@ const ALERTED_FILE = path.join(__dirname, 'alertedSymbols.json');
 // =====================================================================
 const CONFIG = {
     TELEGRAM: {
-        BOT_TOKEN: '7708427979:AAF7vVx6AG8pSyzQU8Xbao87VLhKcbJavdg',
-        CHAT_ID: '-1002554953979',
+        BOT_TOKEN: '7708427979:AAF7vdg',
+        CHAT_ID: '-10029',
         MESSAGE_DELAY_MS: 3000,
         MAX_MESSAGES_PER_MINUTE: 20,
         BURST_DELAY_MS: 5000,
